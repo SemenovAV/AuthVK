2f9b8be0a75fa32c6474721c0f3e3834c6660cbb
Aleksey Semenov <7.on.off@bk.ru>
2020-05-07 16:28:30 +0300

    Исправление
    Изменяет устаревший параметр

--- AuthVK/form_data_handlers/get_captcha_data.py
+++ AuthVK/form_data_handlers/get_captcha_data.py
@@ -4,6 +4,6 @@ def get_captcha_data(ctx: any, params: dict) -> str:
 
     """
     captcha_sid = params.get('captcha_sid')
-    captcha_key = input(f'Enter captcha_key[{ctx.main_uri}/captcha.php?sid={captcha_sid}]: ')
+    captcha_key = input(f'Enter captcha_key[{ctx.main_url}/captcha.php?sid={captcha_sid}]: ')
     if captcha_sid and captcha_key:
         return captcha_key
